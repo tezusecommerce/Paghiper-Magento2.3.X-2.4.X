db1075cf8b244b7f9a0f730b36e1d84574d05682
magento-paghiper <79910070+magento-paghiper@users.noreply.github.com>
2021-03-08 11:27:15 -0300

    Update paghiper_pix.js

--- view/frontend/web/js/view/payment/paghiper_pix.js
+++ view/frontend/web/js/view/payment/paghiper_pix.js
@@ -11,7 +11,7 @@ define(
     rendererList.push(
       {
         type: 'paghiper_pix',
-        component: 'Tezus_PagHiper/js/view/payment/method_renderer/pix_method'
+        component: 'Paghiper_Magento2/js/view/payment/method_renderer/pix_method'
       },
       // other payment method renderers if required
     );
